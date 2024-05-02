22877208f209492b76ab574b46458bfc31ae628e
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2024-05-02 07:29:08 -0300

    Update20240502072900

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('btn-agendar-reparo').addEventListener('click', function() {
+    // Aqui você pode adicionar o código para abrir um modal de agendamento ou redirecionar para uma página de agendamento.
+    // Exemplo: window.location.href = 'pagina_de_agendamento.html';
+    alert('Botão de agendamento clicado!');
+  });
