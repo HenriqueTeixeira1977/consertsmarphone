73ddb0464a9d247fc0b5d928e6ca9a59f0f80a05
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2024-09-23 17:24:07 -0300

    Update2024023172439

--- js/script.js
+++ js/script.js
@@ -18,3 +18,20 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
 	menu.classList.remove('active');
 });
+
+//<!-- GetButton.io widget -->
+//<script type="text/javascript">
+(function () {
+	var options = {
+		whatsapp: "5519982708339", // WhatsApp number
+		call_to_action: "Nos envie sua Mensagem!!!", // Call to action
+		position: "right", // Position may be 'right' or 'left'
+		pre_filled_message: "Ola! Venho do seu Website!!! Preciso de um Orçamento!", // WhatsApp pre-filled message
+	};
+	var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+	var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+	s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+	var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+  })();
+  //</script>
+  //<!-- /GetButton.io widget -->
